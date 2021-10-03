5d72ab27f7c23daa73e68d0ab6ef307247817d67
rafael torres moreno <rafael201_33@hotmail.com>
2021-10-03 17:07:59 -0500

    se modificó settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,4 +126,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# dfff3166b789668f4702f9af3ed3e1a0b031fc77
